01322c5632c417d40053289428b97e28d76eb4b6
hothuuk <kijkj123@gmail.com>
2024-10-22 13:56:57 +0900

    init commit

---
+++ src/main/java/com/ganada/whothefast/WhothefastApplication.java
@@ -0,0 +1,13 @@
+package com.ganada.whothefast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhothefastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhothefastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganada/whothefast/WhothefastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganada.whothefast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhothefastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
