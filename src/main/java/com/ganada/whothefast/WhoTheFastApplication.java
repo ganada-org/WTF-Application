46e23d34b6849dc2144bd2e4fb65c8f8fdb80e7e
hothuuk <kijkj123@gmail.com>
2024-10-22 15:06:46 +0900

    :truck: rename :: Applied CamelCase to filename

--- src/main/java/com/ganada/whothefast/WhoTheFastApplication.java
+++ src/main/java/com/ganada/whothefast/WhoTheFastApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WhothefastApplication {
+public class WhoTheFastApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WhothefastApplication.class, args);
+		SpringApplication.run(WhoTheFastApplication.class, args);
 	}
 
 }
--- src/test/java/com/ganada/whothefast/WhoTheFastApplicationTests.java
+++ src/test/java/com/ganada/whothefast/WhoTheFastApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WhothefastApplicationTests {
+class WhoTheFastApplicationTests {
 
 	@Test
 	void contextLoads() {
