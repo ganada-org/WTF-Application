c9d0a3a266a775bef878be2d2ceaf922436892d2
hothuuk <kijkj123@gmail.com>
2024-10-22 20:15:55 +0900

    :white_check_mark: pass :: create room with required fields

--- src/main/java/com/ganada/whothefast/domain/room/entity/Room.java
+++ src/main/java/com/ganada/whothefast/domain/room/entity/Room.java
@@ -1,6 +1,35 @@
 package com.ganada.whothefast.domain.room.entity;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class Room {
 
+    private String title;
+
+    private int owner;
+
+    private int userCount;
+
+    private int problemDif;
+
+    private String password;
+
+    private List<String> problemTags;
+
+    private int timeLimit;
+
     public Room() { }
+
+    public Room(String title, int owner, int userCount, int problemDif, String password, List<String> problemTags, int timeLimit) {
+        this.title = title;
+        this.owner = owner;
+        this.userCount = userCount;
+        this.problemDif = problemDif;
+        this.password = password;
+        this.problemTags = problemTags;
+        this.timeLimit = timeLimit;
+    }
 }
