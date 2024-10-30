3eb8b2559324742f7b587541dd7570d2bfc78f62
hothuuk <kijkj123@gmail.com>
2024-10-30 18:11:17 +0900

    :test_tube: create :: start room status change test

--- src/test/java/com/ganada/whothefast/room/StartRoomTest.java
+++ src/test/java/com/ganada/whothefast/room/StartRoomTest.java
@@ -13,7 +13,7 @@ public class StartRoomTest {
 
     @Test
     @DisplayName("방 status 값이 START 로 바뀌면 성공")
-    public void room_start() {
+    public void room_status_change() {
         // Given
         Room room = Room.builder()
                 .status(RoomStatus.WAITING)
@@ -21,6 +21,21 @@ public class StartRoomTest {
 
         // When
         room.setStatus(RoomStatus.START);
+
+        // Then
+        assertEquals(RoomStatus.START, room.getStatus());
+    }
+    
+    @Test
+    @DisplayName("방 시작 시 status 값이 START 로 바뀌면 성공")
+    public void start_room_status_change() {
+        // Given
+        Room room = Room.builder()
+                .status(RoomStatus.WAITING)
+                .build();
+
+        // When
+        startRoomService(room);
 
         // Then
         assertEquals(RoomStatus.START, room.getStatus());
